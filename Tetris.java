811f247e29f32c2e71ff9afc4d85e9c6b44bf027
Brandt Davis <brandtd77@gmail.com>
2025-05-21 23:16:42 -0600

    Add basic window drawing functionality

--- Tetris.java
+++ Tetris.java
@@ -1,5 +1,31 @@
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 public class Tetris {
     public static void main(String args[]) {
-        System.out.println("Hello world");
+                // Declaring a Frame and Label
+                Frame frame = new Frame("Basic Program");
+                Label label = new Label("Hello World!");
+        
+                // Aligning the label to CENTER
+                label.setAlignment(Label.CENTER);
+        
+                // Adding Label and Setting
+                // the Size of the Frame
+                frame.add(label);
+                frame.setSize(300, 300);
+        
+                // Making the Frame visible
+                frame.setVisible(true);
+        
+                // Using WindowListener for closing the window
+                frame.addWindowListener(new WindowAdapter() {
+                    @Override
+                    public void windowClosing(WindowEvent e)
+                    {
+                        System.exit(0);
+                    }
+                });
     }
 }
