7124275d77732db6a6e6fb5f16d523cae222836c
Brandt Davis <brandtd77@gmail.com>
2025-05-24 22:52:45 -0600

    Create 2D array to track board state

--- Board.java
+++ Board.java
@@ -1,3 +1,27 @@
 public class Board {
+    protected int[][] boardState;
     
+    public void initializeBoard() {
+        this.boardState = new int[][] {
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        };
+    }
+
+    public void printBoard() {
+        for (int[] boardState1 : boardState) {
+            for (int boardState2 : boardState1) {
+                System.out.print(boardState2 + " ");
+            }
+            System.out.println();
+        }
+    }
 }
