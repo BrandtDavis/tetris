ea33762369db09898bdad8ae3c8dee726c37e308
Brandt Davis <brandtd77@gmail.com>
2025-05-25 05:44:20 -0600

    Replace Tetronimo class with BaseTetronimo class

--- BaseTetronimo.java
+++ BaseTetronimo.java
@@ -1,3 +1,18 @@
-public class BaseTetronimo {
+    // Styles are:
+    // I Piece
+    // O Piece
+    // J Piece
+    // T Piece
+    // Z Piece
+    // L Piece
+    // S Piece
 
+public class BaseTetronimo {
+    public String shapeName;
+    public String color;
+    public int[][] shape;
+
+    public void rotateShape() {
+
+    }
 }
